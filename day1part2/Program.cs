e3d3eaa69525b5117a5bae020b141b1ebed4babf
vmetal123 <bladi22@yahoo.com>
2024-12-18 21:29:14 -0600

    day 1 part 2

---
+++ day1part2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+
+var start = Stopwatch.GetTimestamp();
+
+var lines = File.ReadAllLines("input.txt");
+
+var dict = new Dictionary<string, int>();
+int[] left = [];
+
+foreach (var line in lines)
+{
+    var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    left = [.. left, int.Parse(parts[0])];
+    if (dict.ContainsKey(parts[1]))
+    {
+        dict[parts[1]] += 1;
+    }
+    else
+    {
+        dict[parts[1]] = 1;
+    }
+}
+
+int result = 0;
+for (int i = 0; i < left.Length; i++)
+{
+    if (dict.ContainsKey(left[i].ToString()))
+    {
+        result += left[i] * dict[left[i].ToString()];
+    }
+    else
+    {
+        result += 0;
+    }
+}
+
+Console.WriteLine(result);
+
+var end = Stopwatch.GetElapsedTime(start);
+
+Console.WriteLine(end.TotalMilliseconds);
