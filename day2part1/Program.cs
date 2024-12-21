4f9f0379727a6aa673bd0fc8aac362eabc12360b
vmetal123 <bladi22@yahoo.com>
2024-12-21 16:39:08 -0600

    day 2 part 1

---
+++ day2part1/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+var lines = File.ReadAllLines("input.txt");
+
+int safeReportsCount = 0;
+
+foreach (var line in lines)
+{
+    var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Select(x => int.Parse(x))
+        .ToList();
+
+    bool isDecreasingOrder = parts[0] > parts[1];
+    bool isSafe = true;
+    var first = parts[0];
+
+    for (int i = 1; i < parts.Count; i++)
+    {
+        if (isDecreasingOrder && first < parts[i])
+        {
+            isSafe = false;
+            break;
+        }
+
+        if (!isDecreasingOrder && first > parts[i])
+        {
+            isSafe = false;
+            break;
+        }
+
+        var difference = isDecreasingOrder
+            ? first - parts[i]
+            : parts[i] - first;
+        if (difference == 0 || difference > 3)
+        {
+            isSafe = false;
+            break;
+        }
+        first = parts[i];
+    }
+    if (isSafe)
+        safeReportsCount++;
+}
+
+Console.WriteLine(safeReportsCount);
